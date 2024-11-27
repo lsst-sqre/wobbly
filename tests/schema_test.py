525ee45630ad6a3d40f7b10de1a315f5c4009eb4
Russ Allbery <rra@lsst.org>
2024-11-27 13:34:08 -0800

    Use drop_database from Safir
    Now that Safir provides a `drop_database` utility function, use it
    for the schema test rather than inlining it.

--- tests/schema_test.py
+++ tests/schema_test.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import subprocess
 
 import pytest
-from safir.database import create_database_engine, unstamp_database
+from safir.database import create_database_engine, drop_database
 
 from wobbly.config import config
 from wobbly.schema import SchemaBase
@@ -22,9 +22,7 @@ async def test_schema() -> None:
     engine = create_database_engine(
         config.database_url, config.database_password
     )
-    async with engine.begin() as conn:
-        await conn.run_sync(SchemaBase.metadata.drop_all)
-    await unstamp_database(engine)
+    await drop_database(engine, SchemaBase.metadata)
     await engine.dispose()
     subprocess.run(["alembic", "upgrade", "head"], check=True)
     subprocess.run(["alembic", "check"], check=True)
